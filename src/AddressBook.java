c47028fea23c392398a02b6550aae36dcc3c1205
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-10-05 20:59:26 +0530

    Modify : Indentation of code

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -32,31 +32,31 @@ import java.util.*;
 	
 	
 			
-			//Keeps track of how many address_entries are in the book
-			private int address_entries = 0;
-			PersonAddressDetails[] details;
-			public void initEntries(int e){
-				details = new PersonAddressDetails[e];
-				for (int i = 0;i<details.length;i++){      //Initializes an array of address_entries, then loops through to initialize each individual entry
-					details[i] = new PersonAddressDetails();
-				}
+		//Keeps track of how many address_entries are in the book
+		private int address_entries = 0;
+		PersonAddressDetails[] details;
+		public void initEntries(int e){
+			details = new PersonAddressDetails[e];
+			for (int i = 0;i<details.length;i++){      //Initializes an array of address_entries, then loops through to initialize each individual entry
+				details[i] = new PersonAddressDetails();
 			}
-			public int getEntries(){
-				return details.length;
+		}
+		public int getEntries(){
+			return details.length;
+		}
+
+		//Adds an entry to the book
+		public void add(String first_name, String last_name, String city, String state,int zip, long phoneNo){
+			if (address_entries<details.length){
+				details[address_entries] = new PersonAddressDetails(first_name, last_name, city, state, zip,phoneNo);
+				address_entries++;
+			}else {
+			System.out.println("*************************************************\n"
+			+ "Error: book is full. No insertion will be stored from now on. "
+			+ "\nRe-Execute program and increase number of book address_entries\n"
+			+ "*****************************************************");
 			}
-	
-			//Adds an entry to the book
-			public void add(String first_name, String last_name, String city, String state,int zip, long phoneNo){
-				if (address_entries<details.length){
-					details[address_entries] = new PersonAddressDetails(first_name, last_name, city, state, zip,phoneNo);
-					address_entries++;
-				}else {
-			    	System.out.println("*************************************************\n"
-			    	+ "Error: book is full. No insertion will be stored from now on. "
-			    	+ "\nRe-Execute program and increase number of book address_entries\n"
-			    	+ "*****************************************************");
-				}
-			}
+		}
 		
 			// Removes an entry from the book
 			public void remove(int entry) {
@@ -91,37 +91,37 @@ import java.util.*;
 			for(int i = 0;i<details.length;i++){
 		        for (int j = 0;j<details.length;j++){
 		        switch(n){
-                case 1:
-                    if (details[i].first_name.compareTo(details[j].first_name)<0){
-                        PersonAddressDetails temp = details[i];
-                        details[i] = details[j];
-                        details[j] = temp;
-                    }
-                    break;
-                case 2:
-                    if (details[i].last_name.compareTo(details[j].last_name)<0){
-                        PersonAddressDetails temp = details[i];
-                        details[i] = details[j];
-                        details[j] = temp;
-                    }
-                    break;
-                case 3:
-                    if (details[i].city.compareTo(details[j].city)<0){
-                        PersonAddressDetails temp = details[i];
-                        details[i] = details[j];
-                        details[j] = temp;
-                    }
-                    break;
-                case 4:
-                    if (details[i].phoneNo <0 && details[j].phoneNo<0){
-                	    PersonAddressDetails temp = details[i];
-                        details[i] = details[j];
-                        details[j] = temp;
-                    }
-                    break;
-                default: 
-                    System.out.println("Error: invalid field");
-                        break;
+				case 1:
+				    if (details[i].first_name.compareTo(details[j].first_name)<0){
+					PersonAddressDetails temp = details[i];
+					details[i] = details[j];
+					details[j] = temp;
+				    }
+				    break;
+				case 2:
+				    if (details[i].last_name.compareTo(details[j].last_name)<0){
+					PersonAddressDetails temp = details[i];
+					details[i] = details[j];
+					details[j] = temp;
+				    }
+				    break;
+				case 3:
+				    if (details[i].city.compareTo(details[j].city)<0){
+					PersonAddressDetails temp = details[i];
+					details[i] = details[j];
+					details[j] = temp;
+				    }
+				    break;
+				case 4:
+				    if (details[i].phoneNo <0 && details[j].phoneNo<0){
+					    PersonAddressDetails temp = details[i];
+					details[i] = details[j];
+					details[j] = temp;
+				    }
+				    break;
+				default: 
+				    System.out.println("Error: invalid field");
+				    break;
 		                }
 		            }
 		        }
@@ -138,26 +138,26 @@ import java.util.*;
 	        PersonAddressDetails[] library = new PersonAddressDetails[0];
 
 
-            while(true){
-            howManyPersonAddressDetailss = s.nextInt();
-            if (howManyPersonAddressDetailss>0){
-                library = new PersonAddressDetails[howManyPersonAddressDetailss];                   //This code decides how many books are in the array of books/the library
-                break;
-            }
-            else System.out.print("You must create at least 1 address book.");
-            }
-	
-	        for (int i=0;i<library.length;i++){
-		           library[i] = new PersonAddressDetails(); //Fixed reference to null because each book in the library had not been initialized yet.
-		            while(true){
-	                System.out.print("How many address_entries in book "+i+"? ");
-	                howManyEntries = s.nextInt();
-	                if (howManyEntries>0) {
-	                    library[i].initEntries(howManyEntries);                 //This code decides how many address_entries are in each book in the library
-	                    break;
-	                }
-	                else System.out.println("You must create at least 1 PersonAddressDetails.");
-	                }
+		while(true){
+		howManyPersonAddressDetailss = s.nextInt();
+		if (howManyPersonAddressDetailss>0){
+		library = new PersonAddressDetails[howManyPersonAddressDetailss];                   //This code decides how many books are in the array of books/the library
+		break;
+		}
+		else System.out.print("You must create at least 1 address book.");
+		}
+
+		for (int i=0;i<library.length;i++){
+		   library[i] = new PersonAddressDetails(); //Fixed reference to null because each book in the library had not been initialized yet.
+		   while(true){
+			System.out.print("How many address_entries in book "+i+"? ");
+			howManyEntries = s.nextInt();
+			if (howManyEntries>0) {
+			    library[i].initEntries(howManyEntries);                 //This code decides how many address_entries are in each book in the library
+			    break;
+			}
+			  else System.out.println("You must create at least 1 PersonAddressDetails.");
+			}
 		    }
 	        boolean done = false;
 	        int selectedPersonAddressDetails = 0;
@@ -165,99 +165,96 @@ import java.util.*;
 	        try{
 	        while (done==false){
 
-        	System.out.println("Select an option from below menu");
-            System.out.println("1. Add an entry");
-            System.out.println("2. Remove an entry");
-            System.out.println("3. Edit an entry");
-            System.out.println("4. View PersonAddressDetails PersonAddressDetails");
-            System.out.println("5. Sort address book");
-            System.out.println("5. Select another book");
-            System.out.println("6. Exit the menu");
-            System.out.print("> ");
-           
-            selection = s.nextInt();
-            String first_name, last_name, city, state;
-            int zip;
-			long phoneNo;
+		    System.out.println("Select an option from below menu");
+		    System.out.println("1. Add an entry");
+		    System.out.println("2. Remove an entry");
+		    System.out.println("3. Edit an entry");
+		    System.out.println("4. View PersonAddressDetails PersonAddressDetails");
+		    System.out.println("5. Sort address book");
+		    System.out.println("5. Select another book");
+		    System.out.println("6. Exit the menu");
+		    System.out.print("> ");
 
-            switch(selection){
-            case 1: 
-                System.out.print("First name? ");
-                first_name = s.next();
-                System.out.print("Last name? ");
-                last_name = s.next();
-                System.out.print("city? ");
-                city = s.next();
-                System.out.print("State? ");
-                state = s.next();
-                System.out.print("Zip? ");
-                zip = s.nextInt();
-                System.out.print("phoneNo? ");
-                phoneNo = s.nextInt();
-               
-                library[selectedPersonAddressDetails].add(first_name, last_name, city, state, zip,phoneNo);
-                break;
-            case 2: 
-                System.out.print("Which entry do you want to remove? (Enter the entry number that you want to remove)");
-                int entry = s.nextInt();
-                library[selectedPersonAddressDetails].remove(entry);
-                break;
-            case 3:
-                System.out.print("Enter number of that entry which you want to edit?");
-                int whichPersonAddressDetails = s.nextInt();
-                System.out.print("First name? ");
-                first_name = s.next();
-                System.out.print("Last name? ");
-                last_name = s.next();
-                System.out.print("city? ");
-                city = s.next();
-                System.out.print("State? ");
-                state = s.next();
-                System.out.print("Zip? ");
-                zip = s.nextInt();
-                System.out.print("phoneNo? ");
-                phoneNo = s.nextLong();
-                library[selectedPersonAddressDetails].edit(first_name, last_name, city, state, zip, phoneNo, whichPersonAddressDetails);
-                break;
-            case 4:
-             System.out.println("PersonAddressDetails "+(selectedPersonAddressDetails+1)+" is currently selected.");
-           	 for (int i = 0;i<library[selectedPersonAddressDetails].getEntries();i++){
-           	 System.out.println("===========PersonAddressDetails "+i+" ===========");
-           	 library[selectedPersonAddressDetails].details[i].readPersonAddressDetails(); //Accessing the array of address_entries INSIDE the array of books/the library
-           	 System.out.println("================================");
-           	            }
-           	    break;          
+		    selection = s.nextInt();
+		    String first_name, last_name, city, state;
+		    int zip;
+		    long phoneNo;
+		    switch(selection){
+			    case 1: 
+				System.out.print("First name? ");
+				first_name = s.next();
+				System.out.print("Last name? ");
+				last_name = s.next();
+				System.out.print("city? ");
+				city = s.next();
+				System.out.print("State? ");
+				state = s.next();
+				System.out.print("Zip? ");
+				zip = s.nextInt();
+				System.out.print("phoneNo? ");
+				phoneNo = s.nextInt();
 
-            case 5: 
-                System.out.println("Sort alphabetically by which field?");
-                System.out.println("1. Sort by first_name name");
-                System.out.println("2. Sort by last_name name");
-                System.out.println("3. Sort by city");
-                System.out.println("4. Sort by phoneNo");
-                library[selectedPersonAddressDetails].sort(s.nextInt());
-                break;   
-            case 6: 
-                System.out.print("Select which book?");
-                selectedPersonAddressDetails = s.nextInt();
-                break;
+				library[selectedPersonAddressDetails].add(first_name, last_name, city, state, zip,phoneNo);
+				break;
+			    case 2: 
+				System.out.print("Which entry do you want to remove? (Enter the entry number that you want to remove)");
+				int entry = s.nextInt();
+				library[selectedPersonAddressDetails].remove(entry);
+				break;
+			    case 3:
+				System.out.print("Enter number of that entry which you want to edit?");
+				int whichPersonAddressDetails = s.nextInt();
+				System.out.print("First name? ");
+				first_name = s.next();
+				System.out.print("Last name? ");
+				last_name = s.next();
+				System.out.print("city? ");
+				city = s.next();
+				System.out.print("State? ");
+				state = s.next();
+				System.out.print("Zip? ");
+				zip = s.nextInt();
+				System.out.print("phoneNo? ");
+				phoneNo = s.nextLong();
+				library[selectedPersonAddressDetails].edit(first_name, last_name, city, state, zip, phoneNo, whichPersonAddressDetails);
+				break;
+			    case 4:
+			     System.out.println("PersonAddressDetails "+(selectedPersonAddressDetails+1)+" is currently selected.");
+				 for (int i = 0;i<library[selectedPersonAddressDetails].getEntries();i++){
+				 System.out.println("===========PersonAddressDetails "+i+" ===========");
+				 library[selectedPersonAddressDetails].details[i].readPersonAddressDetails(); //Accessing the array of address_entries INSIDE the array of books/the library
+				 System.out.println("================================");
+					    }
+				    break;          
 
-                
-            case 7:
-                done = true;
-                break;
-           
-            	
-            default:
-                System.out.print("Please choose a valid menu Option");
-            }
-            
-          }
-        }
-          catch(Exception e) {
-        	 
-        	  System.out.println("Please correct input formate by re-executing this programe. \nFirst name Last name and city should be string and phone number should be interger");
-        
-         }
-      }
-    
-    }
+			    case 5: 
+				System.out.println("Sort alphabetically by which field?");
+				System.out.println("1. Sort by first_name name");
+				System.out.println("2. Sort by last_name name");
+				System.out.println("3. Sort by city");
+				System.out.println("4. Sort by phoneNo");
+				library[selectedPersonAddressDetails].sort(s.nextInt());
+				break;   
+			    case 6: 
+				System.out.print("Select which book?");
+				selectedPersonAddressDetails = s.nextInt();
+				break;
+
+
+			    case 7:
+				done = true;
+				break;
+
+
+			    default:
+				System.out.print("Please choose a valid menu Option");
+			}
+		     }
+		  }
+		  catch(Exception e) {
+
+			  System.out.println("Please correct input formate by re-executing this programe. \nFirst name Last name and city should be string and phone number should be interger");
+
+		 }
+	      }
+	    }
